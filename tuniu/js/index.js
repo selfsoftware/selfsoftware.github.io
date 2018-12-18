62681973025090a068d266d13d2395f0879bacf7
self <shaiyaaionle@163.com>
2018-12-18 18:32:36 +0800

    18.32

--- tuniu/js/index.js
+++ tuniu/js/index.js
@@ -1,22 +1,24 @@
 $(function(){
+    // 我的订单
     $(".drop_down").parent().mouseenter(function () { 
         $(".drop_down").show();
         $(this).children("a").children("i").addClass("icon_rotate");
-        $(this).css({
-            backgroundColor:"#fff",
-            
-        })
-        
+        // $(this).css({
+        //     backgroundColor:"#fff",
+        // })
     }).mouseleave(function(){
         $(".drop_down").hide();
         $(this).children("a").children("i").removeClass("icon_rotate");
-        $(this).css({
-            backgroundColor:"#f8f8f8",
-        })
+        // $(this).css({
+        //     backgroundColor:"#f8f8f8",
+        // })
     });
-    $(".hd_map").parent().parent().mouseenter(function(){
+    // 网站地图
+    $(".hd_map").parent().mouseenter(function(){
         $(".hd_map").show();
+        $(this).children("a").children("i").addClass("icon_rotate");
     }).mouseleave(function(){
+        $(this).children("a").children("i").removeClass("icon_rotate");
         $(".hd_map").hide();
     })
 })
