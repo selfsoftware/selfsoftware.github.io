e2a08c53ee59f72c0380f2c69ed7dbb3b0dc90ae
self <shaiyaaionle@163.com>
2018-12-19 18:22:42 +0800

    12.19

--- tuniu/js/index.js
+++ tuniu/js/index.js
@@ -24,4 +24,38 @@ $(function(){
             
         })
     }
+    $(".icon_erweima").parent().mouseenter(function(){
+        $(".icon_erweima").show();
+        $(".icon_weixin").css("background-position","-92px -7px");
+    }).mouseleave(function(){
+        $(".icon_erweima").hide();
+        $(".icon_weixin").css("background-position","-63px -49px");
+    })
+    // 固定的3个导航信息栏
+    $(".left_fix>li:odd").on("click",function(){
+        console.log(this);
+        
+        $(this).addClass("item_hover").siblings().removeClass("item_hover");
+       let index = $(this).index();
+       if($(this).hasClass("item_hover")){
+        switch (index) {
+            case 0:
+                $(this).css("background-color","#dd5a5a");
+                break;
+             case 2:
+             $(this).css("background-color","#eee568");
+                break;
+             case 4:
+             $(this).css("background-color","#b5eea4");
+                break;
+             case 6:
+             $(this).css("background-color","#8bf0d1");
+                break;
+             case 8:
+             $(this).css("background-color","#2da6ec");
+                break;
+        }
+       }
+      
+    })
 })
